122f0170b4a51b3861f667b0596f247a58d9f77a
Daniel Nathan <danieln.szczensny@gmail.com>
2022-10-19 07:24:49 +0200

    🎉: Initial commit

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    internal class Program
+    {
+        public int intToRoman(int number)
+        {
+            int[] num = {1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
+            string[] sym = { "I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M" };
+            
+            int i = num.Length - 1;
+
+            while (number > 0)
+            {
+                int div = number / num[i];
+                number %= num[i];
+
+                for (int j = div; j > 0; j--)
+                {
+                    Console.Write(sym[i]);
+                }
+                i--;
+            }
+            return 0;
+        }
+        
+        public static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.intToRoman(59);
+        }
+    }
+}
